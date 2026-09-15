d9c01463ecc933077a868e26495f577c3f3983ec
Alx <aabcompass@gmail.com>
2026-09-15 23:14:30 +0300

    Automatic translation is turned off

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<span class="text-2xl font-black tracking-wider text-blue-600">Phys<span class="text-gray-800">Rays</span></span>
+<span translate="no" class="text-2xl font-black tracking-wider text-blue-600">Phys<span class="text-gray-800">Rays</span></span>
